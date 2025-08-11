b88d29ab585b49d10469fd50c822bab547351c43
Sebastian Bejm <125sbejm@gmail.com>
2025-08-11 13:48:59 -0700

    add Cart and Product files with MySQL integration

---
+++ ShopEaseApp/Models/Product.cs
@@ -0,0 +1,16 @@
+namespace EventEaseApp.Models {
+    public class Product {
+
+        public int ProductID {get; set;}
+
+        public string Name {get; set;} = string.Empty;
+
+        public decimal Price {get; set;}
+
+        public string Category {get; set;} = string.Empty;
+
+        public string GetProductDetails() {
+            return "Product: " + Name + " | Price: " + Price.ToString("0.##") + " | Category: " + Category;
+        }
+    }
+}
