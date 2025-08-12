bcdb51eaf9acb05b929effb979b5c3a258c1ecec
Sebastian Bejm <125sbejm@gmail.com>
2025-08-12 12:17:02 -0700

    fix quantity issue in Cart

--- ShopEaseApp/Services/CartService.cs
+++ ShopEaseApp/Services/CartService.cs
@@ -2,33 +2,27 @@ using ShopEaseApp.Models;
 
 namespace ShopEaseApp.Services;
 
-public class CartService
-{
+public class CartService {
     private List<Product> cart = new();
 
     public IReadOnlyList<Product> CartItems => cart;
 
-    public void AddProduct(Product product)
-    {
+    public void AddProduct(Product product) {
         cart.Add(product);
     }
 
-    public void RemoveProduct(int productId)
-    {
+    public void RemoveProduct(int productId) {
         var product = cart.FirstOrDefault(p => p.ProductID == productId);
         if (product != null) {
             cart.Remove(product);
         }
     }
 
-    public void SetProductQuantity(Product product, int quantity)
-    {
+    public void SetProductQuantity(Product product, int quantity) {
         cart.RemoveAll(p => p.ProductID == product.ProductID);
 
-        for (int i = 0; i < quantity; i++)
-        {
-            cart.Add(new Product
-            {
+        for (int i = 0; i < quantity; i++) {
+            cart.Add(new Product{
                 ProductID = product.ProductID,
                 Name = product.Name,
                 Price = product.Price,
@@ -36,8 +30,6 @@ public class CartService
             });
         }
     }
-
-
 
     public decimal CalculateTotal()
     {
